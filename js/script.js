24fa167a0a50818eeb4de26b1535a531017d79c0
sannasinne <sanna@keistinen.fi>
2024-01-29 14:20:30 +0200

    Added objects, loops and created new li element

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const button = document.querySelector("button");
+const repairList = document.querySelector(".need-repair");
+
 const createVehicle = function (type, numWheels, color) {
   const vehicle = {
     type: type,
@@ -19,3 +22,21 @@ for (let vehicle of myVehicles) {
     console.log(key, vehicle[key]);
   }
 };
+
+bike.needsRepair = true;
+skateboard.needsRepair = true;
+
+button.addEventListener("click", function () {
+  repairList.innerHTML = "";
+
+  const vehicleRepairList = myVehicles.filter(function (vehicle) {
+    return vehicle.needsRepair === true;
+  });
+
+  for (let vehicle of vehicleRepairList) {
+    let li = document.createElement("li");
+    li.innerHTML = `My <span>${vehicle.type}</span> needs some 💜`;
+    repairList.append(li);
+  }
+});
+
